6748947bd854f20df14714537b48fba5777f332a
Si7A-43 <Si7A-43@DESKTOP-VAKEJFM>
2021-11-11 14:47:14 +0900

    2nd commit

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -1,9 +1,7 @@
-
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("final test");
+		System.out.println("2nd commit");
 	}
-
 }
